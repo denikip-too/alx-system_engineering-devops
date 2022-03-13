068dfb80ee0d865728431ff94bdc6b89e69afcea
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-03-13 14:24:14 -0700

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,12 +3,12 @@
 # Must be configured to use the private key ~/. ssh/school
 include stdlib
 
-file_line { 'USE private key':
+file { 'USE private key':
   path =>  '/etc/ssh/ssh_config',
   line =>  'IdentityFile ~/.ssh/school'
 }
 
-file_line { 'TURN OFF Password Authenticatication':
+file { 'TURN OFF Password Authenticatication':
   path =>  '/etc/ssh/ssh_config',
   line =>  'PasswordAuthentication no'
 }
