5354200af1b4d4697e5eb7b1ac2497a6ab755c63
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-03-13 14:05:36 -0700

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# set up your client SSH configuration file so that you can connect
+# to a server without typing a password
+#Must be configured to use the private key ~/. ssh/school
+include stdlib
+
+file { 'USE private key':
+  path  =>  '/etc/ssh/ssh_config',
+  line  =>  'IdentityFile ~/.ssh/school'
+}
+
+file { 'TURN OFF Password Authenticatication':
+  path  =>  '/etc/ssh/ssh_config',
+  line  =>  'PasswordAuthentication no'
+}
