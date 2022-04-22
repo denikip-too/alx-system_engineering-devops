92a0b17f84d22ea7287c7a29b377e95dc9d21fae
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-04-22 12:03:17 -0700

    fix

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -15,8 +15,8 @@ def todo_list():
 
     with open("{}.json".format(sys.argv[1]), "w") as file:
         json.dump({sys.argv[1]: [{"tasks": task.get('title'),
-                                  "completed": task.get('completed'
-                                      ),"username": employee.get('username')}
+                                  "completed": task.get('completed'),
+                                  "username": employee.get('username')}
                                   for task in todo]}, file)
 
 if __name__ == '__main__':
