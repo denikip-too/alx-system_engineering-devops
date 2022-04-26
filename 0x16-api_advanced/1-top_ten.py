8eb311a0f7ae5353a81682f8ff45afb6e1c636fb
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-04-26 11:53:31 -0700

    prints the titles of the first 10 hot posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""import modules"""
+import requests
+
+def top_ten(subreddit):
+    """queries the Reddit API and prints titles of the first 10 hot posts"""
+    redditAPI = 'https://www.reddit.com/r/{}/hot.json?limit=10'
+    header = {"User-Agent": "My-User-Agent"}
+    reddits = requests.get(
+            redditAPI.format(subreddit), header, allow_redirects=False
+            )
+    if reddits.status_code >= 300:
+        return ("None")
+    else:
+        [print(child.get("data").get("title"))
+        for child in reddits.json()['data']['subscribers']]
