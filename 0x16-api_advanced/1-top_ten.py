12ca718402967a26ab78121201d390db4b4edd6b
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-04-26 12:18:39 -0700

    prints the titles of the first 10 hot posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,13 +4,11 @@ import requests
 
 def top_ten(subreddit):
     """queries the Reddit API and prints titles of the first 10 hot posts"""
-    redditAPI = 'https://www.reddit.com/r/{}/hot.json?limit=10'
+    redditAPI = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
     header = {"User-Agent": "My-User-Agent"}
-    reddits = requests.get(
-            redditAPI.format(subreddit), header, allow_redirects=False
-            )
+    reddits = requests.get(redditAPI, header, allow_redirects=False)
     if reddits.status_code >= 300:
         return ("None")
     else:
         [print(child.get("data").get("title"))
-        for child in reddits.json()['data']['subscribers']]
+        for child in reddits.json()['data']['children']]
