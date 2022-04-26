0837d392c64d0feac6bc997ae18c82345c6352c6
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-04-26 12:23:42 -0700

    returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """queries the Reddit API and returns the number of subscribers"""
-    redditAPI = f'https://www.reddit.com/r/{subreddit}/about.json'
+    redditAPI = 'https://www.reddit.com/r/{subreddit}/about.json'
     header = {"User-Agent": "My-User-Agent"}
     reddits = requests.get(redditAPI, header, allow_redirects=False)
     if reddits.status_code >= 300:
