3996782cdbf85c4400e1dd656db47ffca40091a4
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-04-28 01:16:40 -0700

    fixing Apache is returning a 500 error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # fixing Apache is returning a 500 error
 
 exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
